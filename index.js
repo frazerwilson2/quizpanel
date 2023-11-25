a849d2e0424f4e931d7f8e0b5ad76e769a14a0cc
Frazer Wilson <frazerwilson@Frazers-MacBook-Pro-2.local>
2023-11-25 22:43:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const session = require("express-session");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const sessionMiddleware = session({
+    secret: "changeit",
+    resave: true,
+    saveUninitialized: true,
+});
+
+app.use(sessionMiddleware);
+io.engine.use(sessionMiddleware);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});  
+
+app.get('/admin', (req, res) => {
+    res.sendFile(join(__dirname, 'admin.html'));
+});  
+
+let users = {};
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.request.session.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        var srvSockets = io.sockets.sockets;
+        srvSockets.forEach((value, key) => {delete users[key]});
+        console.log({users});
+        io.emit('userList', users)
+    });
+    socket.on('usr', (user) => {
+        console.log(user);
+        users[user.id] = {name:user.name, answers:{} }
+        io.emit('userList', users)
+    });
+    socket.on('answers', (msg) => {
+        console.log(msg);
+        const newUsers = Object.assign({}, users);
+        newUsers[msg.id].answers = msg.answers;
+        users = newUsers;
+        io.emit('userList', users)
+    });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
